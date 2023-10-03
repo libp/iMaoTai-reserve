5262a80a341ad5a65962219407477944515d127a
Li Bp <924701358@qq.com>
2023-10-03 09:55:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ for section in configs.sections():
                 s_title = '！！失败！！茅台预约'
             s_content = s_content + r_content + shopInfo + "\n"
             # 领取小茅运和耐力值
-            process.getUserEnergyAward(mobile)
+            # process.getUserEnergyAward(mobile)
     except BaseException as e:
         print(e)
         logging.error(e)
